857bc230000b30f472c12eb8abd5b8d6658f7b8a
AkiaWilliams <59836109+AkiaWilliams@users.noreply.github.com>
2020-03-03 11:24:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../src/App.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import Fetch from "./components/Fetch";
 const App = () => {
   return (
     <div>
       <NavBar />
       <Home />
+      <Fetch />
     </div>
   );
 };
